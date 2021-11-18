129b5d016a6051004c7404a32196f8ed3d846905
Pavel <panteleevofficial@yandex.ru>
2021-11-18 09:13:18 +0300

    Navigation

--- app/src/main/java/com/example/likeadesigner/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/likeadesigner/ui/main/PictureOfTheDayFragment.kt
@@ -1,25 +1,25 @@
 package com.example.likeadesigner.ui.main
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
 import com.example.likeadesigner.R
 import com.example.likeadesigner.databinding.MainFragmentBinding
+import com.example.likeadesigner.ui.main.caroosel.ApiActivity
 import com.example.likeadesigner.view.util.hide
 import com.example.likeadesigner.view.util.show
 import com.example.likeadesigner.viewmodel.PictureOfTheDayData
 import com.example.likeadesigner.viewmodel.PictureOfTheDayViewModel
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class PictureOfTheDayFragment : Fragment() {
 
     private val viewModel: PictureOfTheDayViewModel by lazy {
-        ViewModelProvider(requireActivity()).get(PictureOfTheDayViewModel::class.java)
+        ViewModelProvider(requireActivity())[PictureOfTheDayViewModel::class.java]
     }
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
@@ -36,6 +36,11 @@ class PictureOfTheDayFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getData()
             .observe(viewLifecycleOwner, { renderData(it) })
+        for (i in 0 until bottom_navigation.menu.size()) {
+            bottom_navigation.menu.getItem(i).setOnMenuItemClickListener {
+                onOptionsItemSelected(it)
+            }
+        }
     }
 
     private fun renderData(data: PictureOfTheDayData) {
@@ -61,9 +66,24 @@ class PictureOfTheDayFragment : Fragment() {
                 binding.mainFragmentLoadingLayout.show()
             }
             is PictureOfTheDayData.Error -> {
+                toast("I have a trouble")
             }
 
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.view_pager_button -> activity?.let {
+                startActivity(
+                    Intent(
+                        it,
+                        ApiActivity::class.java
+                    )
+                )
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun Fragment.toast(string: String?) {
